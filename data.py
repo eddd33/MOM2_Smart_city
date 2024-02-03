11883e8e5ad7ab10385a3f03ba94d21620852fb1
eddd33 <edgar.peronnin@telecomnancy.eu>
2024-02-03 16:11:52 +0100

    [add] generation of the previous consumptions and prediction of tomorrow consumption

---
+++ data.py
@@ -0,0 +1,49 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def get_consumption_yesterday():
+    # on retourne la consommation d'hier de chaque communauté
+    # pour chaque communauté, on retourne une liste de 24 valeurs
+    # chaque valeur est la consommation en kWh pour une heure de la journée
+
+    n_communautes = 8
+    n_maisons = 1000
+    heures_pleines = [8,9,10,11,12,13,14,20,21]
+    heures_creuses = [1,2,3,4,5]
+
+    # la consommation d'une maison est comprise entre 5 et 15 kWh par jour
+    # on suppose que la consommation est plus élevée en heure pleine
+
+    # pour chaque heure de la journée, on retourne la consommation de chaque maison
+    consommation = []
+    for i in range(24):
+        if i in heures_pleines:
+            conso_maisons = np.random.uniform(0.24, 0.56, n_maisons)*1.1
+        elif i in heures_creuses:
+            conso_maisons = np.random.uniform(0.24, 0.56, n_maisons)*0.9
+        else:
+            conso_maisons = np.random.uniform(0.24, 0.56, n_maisons)
+        consommation.append(conso_maisons.sum())
+     
+    return consommation
+
+
+def pred_consumtion():
+    # on retourne la prédiction de la consommation pour chaque communauté
+
+    conso_hier = get_consumption_yesterday()
+
+    # Lissage avec une moyenne mobile sur une fenêtre de 3 heures (vous pouvez ajuster la taille de la fenêtre)
+    fenetre = 3
+    prediction = np.convolve(conso_hier, np.ones(fenetre)/fenetre, mode='same')
+    prediction[0] = conso_hier[0]
+    prediction[-1] = conso_hier[-1]
+    # graphique
+    plt.plot(conso_hier, label="hier")
+    plt.plot(prediction, label="prédiction")
+    plt.legend()
+    plt.show()
+
+    return prediction
+
+print(pred_consumtion())
--- hems.py
+++ hems.py
@@ -9,8 +9,21 @@ class DemandAgent(Agent):
     def __init__(self, unique_id, model):
         super().__init__(unique_id, model)
         self.mode = np.random.choice(['vert', 'stable', 'bas coût'])
+        self.consommation = 0
     def step(self):
-        pass
+        # randomisation de la consommation entre 0.21 et 0.625 car une maison consomme entre 5 et 15 kWh par jour
+        self.consommation = np.random.uniform(0.21, 0.625)
+        # Ajustement de la consommation en fonction du mode
+        if self.mode == 'vert':
+            # self.consommation = np.random.uniform(0.21, 0.50)
+            pass
+        elif self.mode == 'stable':
+            # on ne veut pas de consommation trop faible
+            pass
+        elif self.mode == 'bas coût':
+            # on utilise de l'électricité lorsqu'elle est moins chère
+            pass 
+
 
 class GenerationAgent(Agent):
     # génère de l'électricité
@@ -32,12 +45,15 @@ class BatteryAgent(Agent):
         self.capacity = self.capacity + np.random.normal(0, 25)
         if self.capacity < 0:
             self.capacity = 0
+        if self.capacity > 100:
+            self.capacity = 100
 
 ####################################################################################################
             
-class HEMS(Model):
+class HEMS(Agent):
     # modèle de la maison
-    def __init__(self):
+    def __init__(self, unique_id, model):
+        super().__init__(unique_id, model)
         self.schedule = RandomActivation(self)
         # création des agents
         a = DemandAgent(1, self)
@@ -51,10 +67,10 @@ class HEMS(Model):
         self.schedule.step()
 
 
-# test
-h = HEMS()
-h.step()
+# # test
+# h = HEMS(1)
+# h.step()
 
-print(h.schedule.agents[0].mode)
-print(h.schedule.agents[1].generation)
-print(h.schedule.agents[2].capacity)
+# print(h.schedule.agents[0].mode)
+# print(h.schedule.agents[1].generation)
+# print(h.schedule.agents[2].capacity)
